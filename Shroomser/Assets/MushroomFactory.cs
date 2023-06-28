4047605f3e0998dca02c9ff12ba68162e309b8f0
HoMka0IQ <71536727+HoMka0IQ@users.noreply.github.com>
2023-06-28 16:26:34 +0300

    д

--- Shroomser/Assets/MushroomFactory.cs
+++ Shroomser/Assets/MushroomFactory.cs
@@ -6,15 +6,42 @@ using TMPro;
 public class MushroomFactory : MonoBehaviour
 {
     public TMP_Text number;
+    Basket basket;
+
     public int MaxCount;
     public List<ItemMushroom> mushroomsInFactory;
-    Basket basket;
+
+    public GameObject boxPrefab;
+    public Transform SpawnBoxPosition;
+    [HideInInspector]public bool isWorking = false;
+    public float timerInSeconds;
+    float _currentTimer = 0;
+   
     private void Start()
     {
         basket = Basket.instance;
         TextReload();
     }
 
+    private void Update()
+    {
+        if (isWorking)
+        {
+            _currentTimer += Time.deltaTime;
+            if (_currentTimer >= timerInSeconds)
+            {
+                InstantiateMushroomBox();
+                isWorking = false;
+                _currentTimer = 0;
+            }
+        }
+    }
+    public void InstantiateMushroomBox()
+    {
+        Instantiate(boxPrefab, SpawnBoxPosition.position, Quaternion.identity);
+        mushroomsInFactory.Clear();
+        TextReload();
+    }
     public void TextReload()
     {
         number.text = mushroomsInFactory.Count + "/" + MaxCount;
@@ -32,6 +59,12 @@ public class MushroomFactory : MonoBehaviour
             mushroomsInFactory.Add(basket.mushroomsInBasket[basket.mushroomsInBasket.Count - 1]);
             basket.mushroomsInBasket.RemoveAt(basket.mushroomsInBasket.Count - 1);
             TextReload();
+            if (MaxCount == mushroomsInFactory.Count)
+            {
+                isWorking = true;
+            }
         }
     }
+
+
 }
